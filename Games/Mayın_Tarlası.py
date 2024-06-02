68873e67e27b6eea8410f9ff39a1a2af11faa104
CanPerest019 <ahbcr1919@gmail.com>
2024-06-02 15:38:17 +0300

    Python-Pygame
    Upgrade V2.1

--- Games/Mayın_Tarlası.py
+++ Games/Mayın_Tarlası.py
@@ -2,28 +2,23 @@ import pygame
 import random
 import sys
 
-# Ekran boyutu ve hücre boyutu
 HUCRE_BOYUTU = 20
 HUCRE_SAYISI = 20
 EKRAN_BOYUTU = HUCRE_BOYUTU * HUCRE_SAYISI
 
-# Mayın sayısı
 MAYIN_SAYISI = 40
 
-# Renkler
 BEYAZ = (255, 255, 255)
 SIYAH = (0, 0, 0)
 GRI = (192, 192, 192)
 KIRMIZI = (255, 0, 0)
 
-# Pygame'i başlat
 pygame.init()
 ekran = pygame.display.set_mode((EKRAN_BOYUTU, EKRAN_BOYUTU))
 pygame.display.set_caption("Mayın Tarlası")
 saat = pygame.time.Clock()
 font = pygame.font.SysFont(None, 24)
 
-# Mayın Tarlası oluştur
 def mayin_tarlasi_olustur():
     tarla = [[0 for _ in range(HUCRE_SAYISI)] for _ in range(HUCRE_SAYISI)]
     mayinlar = random.sample(range(HUCRE_SAYISI * HUCRE_SAYISI), MAYIN_SAYISI)
@@ -37,7 +32,6 @@ def mayin_tarlasi_olustur():
                     tarla[i][j] += 1
     return tarla
 
-# Hücreleri açma
 def hucreleri_ac(tarla, gorunen, x, y):
     if gorunen[x][y]:
         return
@@ -48,13 +42,11 @@ def hucreleri_ac(tarla, gorunen, x, y):
                 if not gorunen[i][j]:
                     hucreleri_ac(tarla, gorunen, i, j)
 
-# Oyunu başlat
 def oyun_baslat():
     global tarla, gorunen
     tarla = mayin_tarlasi_olustur()
     gorunen = [[False for _ in range(HUCRE_SAYISI)] for _ in range(HUCRE_SAYISI)]
 
-# Oyun döngüsü
 oyun_baslat()
 while True:
     for event in pygame.event.get():
@@ -76,7 +68,6 @@ while True:
     # Ekranı temizle
     ekran.fill(BEYAZ)
 
-    # Hücreleri çiz
     for x in range(HUCRE_SAYISI):
         for y in range(HUCRE_SAYISI):
             rect = pygame.Rect(x * HUCRE_BOYUTU, y * HUCRE_BOYUTU, HUCRE_BOYUTU, HUCRE_BOYUTU)
@@ -96,4 +87,4 @@ while True:
                 pygame.draw.rect(ekran, SIYAH, rect, 1)
 
     pygame.display.flip()
-    saat.tick(30)  # Oyun hızı
+    saat.tick(30)  
